d8b4964528f2a1b80d31b60cafc220ca62b06164
TakumiOda <xiaotiantakumi@gmail.com>
2023-05-27 20:07:16 +0900

    fix SampleTrigger/index.ts

--- SampleTrigger/index.ts
+++ SampleTrigger/index.ts
@@ -11,7 +11,7 @@ const httpTrigger: AzureFunction = async function (
     : "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response.test";
 
   context.res = {
-    // status: 200, /* Defaults to 200 */
+    status: 200,
     body: responseMessage,
   };
 };
